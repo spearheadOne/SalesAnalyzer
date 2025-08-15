836212516720204864b9580fc5448e306d5be2c0
Alex Bondar <abondar1992@gmail.com>
2025-08-15 12:26:44 +0200

    kinesis client

--- SalesIngestor/src/main/java/org/abondar/experimental/sales/analyzer/ingestor/IngestionPublisher.kt
+++ SalesIngestor/src/main/java/org/abondar/experimental/sales/analyzer/ingestor/IngestionPublisher.kt
@@ -1,12 +1,40 @@
 package org.abondar.experimental.sales.analyzer.ingestor
 
+import io.micronaut.context.annotation.Value
+import io.micronaut.serde.ObjectMapper
 import jakarta.inject.Singleton
+import kotlinx.coroutines.future.await
 import org.abondar.exerimental.sales.analyzer.data.SalesRecord
+import software.amazon.awssdk.core.SdkBytes
+import software.amazon.awssdk.services.kinesis.KinesisAsyncClient
+import software.amazon.awssdk.services.kinesis.model.PutRecordsRequest
+import software.amazon.awssdk.services.kinesis.model.PutRecordsRequestEntry
 
 @Singleton
-class IngestionPublisher {
+class IngestionPublisher(
+    private val mapper: ObjectMapper,
+    private val kinesisClient: KinesisAsyncClient
+) {
 
-    suspend fun publishMessage(batch: List<SalesRecord>){
+    @Value("\${aws.kinesis.stream}")
+    lateinit var streamName: String
+
+    suspend fun publishMessage(batch: List<SalesRecord>) {
+        val entries = batch.map { record ->
+            val data = mapper.writeValueAsBytes(record)
+            PutRecordsRequestEntry.builder()
+                .data(SdkBytes.fromByteArray(data))
+                .partitionKey(record.orderId)
+                .build()
+        }
+
+        val req = PutRecordsRequest.builder()
+            .streamName(streamName)
+            .records(entries)
+            .build()
+
+        kinesisClient.putRecords(req).await()
 
     }
+
 }
