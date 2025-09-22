2b82f5781dfdf3637fce52dd580f5e345b9283dd
Alex Bondar <abondar1992@gmail.com>
2025-09-22 20:27:03 +0200

    rename in init

--- SalesDashboard/src/test/resources/sql/init-db.sql
+++ SalesDashboard/src/test/resources/sql/init-db.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS sales_agg (
-                                         bucket_start_time TIMESTAMPTZ NOT NULL,
+                                         event_time TIMESTAMPTZ NOT NULL,
                                          product_id   VARCHAR(255) NOT NULL,
                                          product_name VARCHAR(255) NOT NULL ,
                                          category     VARCHAR(255) NOT NULL,
                                          orders       BIGINT NOT NULL,
                                          units        BIGINT NOT NULL,
                                          revenue      DOUBLE PRECISION NOT NULL,
-                                         CONSTRAINT pk_sales_agg PRIMARY KEY (bucket_start_time, product_id)
+                                         CONSTRAINT pk_sales_agg PRIMARY KEY (event_time, product_id)
 );
