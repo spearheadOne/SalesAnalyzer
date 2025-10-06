98fe2ff31a3dd5a4e5a1a57b6b5b9de6727f5114
Alex Bondar <abondar1992@gmail.com>
2025-10-06 20:11:07 +0200

    base app components + app layout

---
+++ SalesDashboard/dashboard-frontend/src/components/History.tsx
@@ -0,0 +1,9 @@
+function History() {
+    return (
+        <>
+            <h1>Historic data</h1>
+        </>
+    )
+}
+
+export default History
---
+++ SalesDashboard/dashboard-frontend/src/components/Live.tsx
@@ -0,0 +1,9 @@
+function Live() {
+    return (
+        <>
+            <h1>Live data from stream</h1>
+        </>
+    )
+}
+
+export default Live
