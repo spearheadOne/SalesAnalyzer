a596a1d4ec29cebf55cbb4a0aa8a5f4d07c56b3e
Alex Bondar <abondar1992@gmail.com>
2025-10-10 01:53:13 +0200

    fetch button component

---
+++ SalesDashboard/dashboard-frontend/src/components/FetchButton.tsx
@@ -0,0 +1,22 @@
+
+
+interface FetchButtonProps {
+    fetchData: () => void | Promise<void>;
+}
+
+
+export default function FetchButton({fetchData}: FetchButtonProps) {
+
+    const handleClick = async () => {
+        await fetchData();
+
+    }
+
+    return (
+        <button className="btn btn-primary"
+                type="submit"
+                onClick={handleClick}>
+            Get data
+        </button>
+    )
+}
--- SalesDashboard/dashboard-frontend/src/components/TopCategoriesChart.tsx
+++ SalesDashboard/dashboard-frontend/src/components/TopCategoriesChart.tsx
@@ -1,15 +1,18 @@
 import Period from "./Period.tsx";
 import Limit from "./Limit.tsx";
+import FetchButton from "./FetchButton.tsx";
+import {useHistoricDataStore} from "../store/historicDataStore.ts";
 
 export default function TopCategoriesChart() {
 
     // const categoryResponse = useHistoricDataStore((state) => state.categoryResponse);
-    // const fetchCategoryRevenue = useHistoricDataStore((state) => state.fetchCategoryRevenue);
+     const fetchCategoryRevenue = useHistoricDataStore((state) => state.fetchCategoryRevenue);
 
     return (
         <>
             <Period/>
             <Limit/>
+            <FetchButton fetchData={()=>fetchCategoryRevenue("1h",10)}/>
         </>
     )
 
