6752f7ed2308790c65662db3fd6d8b2d6276011f
Alex Bondar <abondar1992@gmail.com>
2025-12-28 22:21:54 +0100

    restore outputs, fix some configs

--- infra/core/outputs.tf
+++ infra/core/outputs.tf
@@ -1,6 +1,23 @@
+output "sales_bucket_name" {
+  description = "Actual created S3 bucket name (globally unique)"
+  value       = aws_s3_bucket.sales_data.bucket
+}
+
 output "upload_invoke_url" {
   description = "API gateway URL for data upload"
   value = "https://${aws_api_gateway_rest_api.upload_api.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.stage.stage_name}/upload/{filename}"
+}
+
+output "sqs_queue_url" {
+  value = aws_sqs_queue.sales_queue.url
+}
+
+output "sqs_queue_name" {
+  value = aws_sqs_queue.sales_queue.name
+}
+
+output "kinesis_stream_name" {
+  value = aws_kinesis_stream.sales_stream.name
 }
 
 output "sales_ingester_ecr_url" {
